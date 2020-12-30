82f22c1854da55ed33bf171d3dad63e362fbd128
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2020-12-31 03:37:26 +0800

    Change design

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-function changeBanner(imageURL, bannerBodyHTML, platform, writtenIn){
-    document.getElementById("banner-image").src = imageURL;
-    document.getElementById("banner-body").innerHTML = bannerBodyHTML;
-    document.getElementById("banner-platform").innerHTML = platform;
-    document.getElementById("banner-written-in").innerHTML = writtenIn;
-}
-
 window.onload = function(){
     
 };

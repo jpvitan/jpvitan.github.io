8a9ddc0f4874cfa698d0f7798830a2b9e2105346
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-05 22:22:19 +0800

    Add mail function to contact form

--- contact_form.php
+++ contact_form.php
@@ -1,3 +1,22 @@
 <?php
-    echo 'Hello, World!';
+    if(isset($_POST['submit'])){
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $country = $_POST['country'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        
+        $destination = 'justinepaulvitan@jpvitan.com';
+        $headers = 'From: '.$email;
+        
+        $message = '[Sender Details]\nFirst Name: '.$first_name.'\nLast Name: '.$last_name.'\nCountry: '.$country.'\n\n[Message]\n'.$message;
+        
+        $result = mail($destination, $subject, $message, $headers);
+        if($result){
+            
+        }else{
+            
+        }
+    }
 ?>

eec17c5b3502ea5cc7d0146b206a0e3332533202
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-08 03:33:04 +0800

    Add second banner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 AOS.init();
 
-window.onload = function(){
+var bannerIndex = 0;
+var bannerImage = ["img/banner/1.png", "img/banner/2.png"];
+var bannerText = ["*Google Play and the Google Play logo are trademarks of Google LLC.", "github.com/jpvitan/rsa-jpv"];
+
+function updateBanner(){
+    document.getElementById("bannerImage").src = bannerImage[bannerIndex];
+    document.getElementById("bannerText").innerHTML = bannerText[bannerIndex];
     
+    bannerIndex++;
+    if(bannerIndex > bannerImage.length - 1){
+        bannerIndex = 0;
+    }
+    setTimeout(updateBanner, 8000);
+}
+
+window.onload = function(){
+    updateBanner();
 };

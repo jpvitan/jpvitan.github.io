f2d872ee01109dbec19ca717c718667e29a0983b
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-09 02:07:37 +0800

    Add portfolio page

---
+++ portfolio.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>JPVitan | Portfolio</title>   
+        
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        
+        <!-- Meta Tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Justine Paul Vitan is software developer who specializes in building mobile and web applications.">
+        <meta name="author" content="Justine Paul Vitan">
+        
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        
+        <!-- AOS -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+        
+        <!-- CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/portfolio.css" rel="stylesheet">
+    </head>
+    <body>    
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm style-navbar" id="navbar"></nav>
+
+        <script>
+        $(function(){
+            $("#navbar").load("navbar.html");    
+        });
+        </script>
+
+        <div style="height: 90px;"></div>
+                
+        <div class="container-fluid py-4">
+            <div class="row justify-content-center text-center">
+                <div class="col-sm-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#747d8c" class="bi bi-folder-fill" viewBox="0 0 16 16">
+                        <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
+                    </svg>
+                    <h1 class="style-portfolio-heading mt-2">Portfolio</h1>
+                    <p class="style-portfolio-note"></p>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center">
+                <div class="col-lg-2">
+                    <input class="form-control mt-2" type="text" id="search" name="search" placeholder="Search" required>
+                </div>
+                <div class="col-lg-2">
+                    <select class="form-select mt-2" id="platform" name="platform">
+                        <option value="">All Platforms</option>
+                        <option value="Mobile">Mobile</option>
+                        <option value="Web">Web</option>
+                        <option value="Desktop">Desktop</option>
+                        <option value="Others">Others</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+                <div class="col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="img/banner/1.png">
+                        <div class="card-body">
+                            <h5 class="card-title">SatTrack-X</h5>
+                            <p class="card-text">Satellite tracker app for Android with custom map themes and 2000+ trackable objects.</p>
+                            <div>Technologies Used: Java, Android SDK</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+                <div class="col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="img/banner/2.png">
+                        <div class="card-body">
+                            <h5 class="card-title">rsa-jpv</h5>
+                            <p class="card-text">A simple Python library that encrypts your data using the RSA cryptosystem.</p>
+                            <div>Technologies Used: Python</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <footer class="my-2 text-center style-footer">
+            <div class="container-fluid">Developed and Designed by Justine Paul Sanchez Vitan.</div>
+            <div class="container-fluid">Copyright © 2021 Justine Paul Sanchez Vitan. All rights reserved.</div>
+        </footer>
+        
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        
+        <!-- AOS -->
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        
+        <!-- JavaScript -->
+        <script src="js/portfolio.js"></script>
+    </body>
+</html>

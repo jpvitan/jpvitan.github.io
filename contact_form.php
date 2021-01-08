824560088c11c151aa3d424e18fd0ff6ca8b4a82
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-09 06:18:02 +0800

    Fix echo formatting

--- contact_form.php
+++ contact_form.php
@@ -58,21 +58,21 @@
                             $result = mail($destination, $subject, $message, $headers);
                             if($result){
                                 echo "
-                                 <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='#2ecc71' class='bi bi-check-circle-fill mt-4' viewBox='0 0 16 16'>
-                        <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
-                    </svg>
-                    <h1 class='mt-2' style='color: #747d8c;'>Your message has been sent!</h1>
-                    <div class='mt-2' style='color: #485460;'>Hi ".$first_name."! Thanks for your message.</div>
-                    <div style='color: #485460;'>Please expect a reply within 48 hours after you have received this message.</div>
+                                <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='#2ecc71' class='bi bi-check-circle-fill mt-4' viewBox='0 0 16 16'>
+                                    <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
+                                </svg>
+                                <h1 class='mt-2' style='color: #747d8c;'>Your message has been sent!</h1>
+                                <div class='mt-2' style='color: #485460;'>Hi ".$first_name."! Thanks for your message.</div>
+                                <div style='color: #485460;'>Please expect a reply within 48 hours after you have received this message.</div>
                                 ";
                             }else{
                                 echo "
                                 <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='#ff3f34' class='bi bi-x-circle-fill mt-4' viewBox='0 0 16 16'>
-                        <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/>
-                    </svg>
-                    <h1 class='mt-2' style='color: #747d8c;'>Your message has not been sent!</h1>
-                    <div class='mt-2' style='color: #485460;'>Hi ".$first_name."! Thanks for your message.</div>
-                    <div style='color: #485460;'>Unfortunately, your message was not sent. You may message me instead on my social media accounts.</div>
+                                    <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/>
+                                </svg>
+                                <h1 class='mt-2' style='color: #747d8c;'>Your message has not been sent!</h1>
+                                <div class='mt-2' style='color: #485460;'>Hi ".$first_name."! Thanks for your message.</div>
+                                <div style='color: #485460;'>Unfortunately, your message was not sent. You may message me instead on my social media accounts.</div>
                                 ";
                             }
                         }

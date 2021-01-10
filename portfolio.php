850dbcd4a537d7d35bbecdd7ce25d1c6549240d5
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-10 19:32:40 +0800

    Update design

--- portfolio.php
+++ portfolio.php
@@ -70,16 +70,16 @@
             </form>
             <?php    
                 // For production server:
-//                $host = "localhost";
-//                $username = "jpvitan1_master";
-//                $password = "!M,xxii*MKRR";
-//                $dbname = "jpvitan1_site";
+                $host = "localhost";
+                $username = "jpvitan1_master";
+                $password = "!M,xxii*MKRR";
+                $dbname = "jpvitan1_site";
             
                 // For local server:
-                $host = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "jpvitan_db";
+//                $host = "localhost";
+//                $username = "root";
+//                $password = "";
+//                $dbname = "jpvitan_db";
             
                 $mysqli = new mysqli($host, $username, $password, $dbname);
                 if($mysqli->connect_errno){
@@ -115,12 +115,12 @@
                     <div class='row justify-content-center mt-4'>
                         <div class='col-lg-4'>
                             <a href='". $link ."' style='text-decoration: none; color: black;'>
-                                <div class='card'>
+                                <div class='card shadow border-0'>
                                     <img class='card-img-top' src='" . $image_banner . "'>
                                     <div class='card-body'>
                                         <h5 class='card-title'>". $title ."</h5>
                                         <p class='card-text'>". $description ."</p>
-                                        <div>Technologies Used: ". $technologies_used ."</div>
+                                        <div style='color: #808e9b;'>Technologies Used: ". $technologies_used ."</div>
                                     </div>
                                 </div>
                             </a>

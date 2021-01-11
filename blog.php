8ab282ab70822d63771184cf9506f79eed257c39
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-12 02:19:25 +0800

    Preload fonts

--- blog.php
+++ blog.php
@@ -28,6 +28,7 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>      
         
         <!-- CSS -->
+        <link rel="preload" as="font" href="css/font/lemonmilk.otf" crossorigin="anonymous">
         <link href="css/style.css" rel="stylesheet">
         <link href="css/blog.css" rel="stylesheet">
     </head>

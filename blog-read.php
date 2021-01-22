bd116f66b58762564eb10192b57f0792516afe8e
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-01-23 05:09:14 +0800

    Add blog functionality

---
+++ blog-read.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>JPVITAN - About</title>   
+        
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        
+        <!-- Meta Tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Justine Paul Vitan (jpvitan) is a software developer who builds mobile, web, and desktop applications.">
+        <meta name="author" content="Justine Paul Vitan">
+        
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        
+        <!-- AOS -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>      
+        
+        <!-- CSS -->
+        <link rel="preload" as="font" href="css/font/lemonmilk.otf" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/about.css" rel="stylesheet">
+    </head>
+    <body>    
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top style-navbar" id="navbar"></nav>
+
+        <script>
+        $(function(){
+            $("#navbar").load("navbar.html");    
+        });
+        </script>
+
+        <div style="height: 74px;"></div>
+        
+        <div class="container mt-lg-5">
+            <div class="row justify-content-center">
+                <?php
+                if (isset($_GET["id"])){
+                    
+                }else{
+                    
+                }
+                ?>
+            </div>
+        </div>
+        
+        <footer class="my-2 text-center style-footer" id="footer">
+            
+        </footer>
+        
+        <script>
+        $(function(){
+            $("#footer").load("footer.html");
+        });
+        </script>
+        
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        
+        <!-- JavaScript -->
+        <script src="js/about.js"></script>
+    </body>
+</html>
--- blog.php
+++ blog.php
@@ -73,23 +73,64 @@
                 </div>
             </form>
             <?php    
-                $host = "localhost";
-                $dbname = "jpvitan1_site";
-    
-                // For production server:
-                $username = "jpvitan1_master";
-                $password = "!M,xxii*MKRR";
-            
-                // For local server:
-//                $username = "root";
-//                $password = "";
-                        
-                $mysqli = new mysqli($host, $username, $password, $dbname);
-                if($mysqli->connect_errno){
-                    echo "<div class='row justify-content-center text-center mt-4'><h1 style='color: #ff3f34;'>Connection Failed!</h1></div>";
-                }else{
-                    
+            $host = "localhost";
+            $dbname = "jpvitan1_site";
+
+            // For production server:
+            $username = "jpvitan1_master";
+            $password = "!M,xxii*MKRR";
+
+            // For local server:
+            $username = "root";
+            $password = "";
+
+            $mysqli = new mysqli($host, $username, $password, $dbname);
+            if($mysqli->connect_errno){
+                echo "<div class='row justify-content-center text-center mt-4'><h1 style='color: #ff3f34;'>Connection Failed!</h1></div>";
+            }else{                
+                $statement = $mysqli->prepare("SELECT * FROM blog");
+                
+                if(isset($_GET["title"]) && isset($_GET["category"])){
+                    $title = $_GET["title"];
+                    $category = $_GET["category"];
+                    if($title != "" && $category != "All"){
+                        $statement = $mysqli->prepare("SELECT * FROM blog WHERE title LIKE ? AND category=?");
+                        $title .= "%";
+                        $statement->bind_param("ss", $title, $category);
+                    }
+                    else if($title != ""){
+                        $statement = $mysqli->prepare("SELECT * FROM blog WHERE title LIKE ?");
+                        $title .= "%";
+                        $statement->bind_param("s", $title);
+                    }
+                    else if($category != "All"){
+                        $statement = $mysqli->prepare("SELECT * FROM blog WHERE category=?");
+                        $statement->bind_param("s", $category);
+                    }
                 }
+
+                $statement->execute();
+                $statement->bind_result($id, $image_banner, $title, $description, $author, $date, $category, $link);
+
+                while($statement->fetch()){
+                    echo "
+                    <div class='row justify-content-center mt-4'>
+                        <div class='col-lg-4'>
+                            <a href='blog-read.php?id=". $id ."' style='text-decoration: none; color: black;'>
+                                <div class='card shadow border-0'>
+                                    <img class='card-img-top' alt='Banner' src='" . $image_banner . "'>
+                                    <div class='card-body'>
+                                        <div class='card-title' style='font-size: 1.25rem; font-weight: 500; margin-bottom: 0px;'>". $title ."</div>
+                                        <div style='color: #747d8c; font-size: 0.9rem;'>" . $author . ", " . $date ."</div>
+                                        <p style='margin-top: 1rem;'>". $description ."</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    ";
+                }
+            }
             ?>
         </div>
         

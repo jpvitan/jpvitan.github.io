52f86ffee428fdc9afde06695c252876e3a38373
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-02-25 03:55:08 +0800

    Update redirect contact-form

--- js/contact-form.js
+++ js/contact-form.js
@@ -4,7 +4,7 @@ window.onload = function(){
     var button = document.getElementById("button");
     button.onclick = function(){
         if (button.value == 0){
-            location.href = "index.html";
+            location.href = "/";
         }else if(button.value == 1){
             history.go(-1);
         }    

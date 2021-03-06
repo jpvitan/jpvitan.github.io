8bcd778b56971121bc448971be73bf5324680a83
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-03-06 19:19:27 +0800

    Update portfolio and blog design

--- blog.php
+++ blog.php
@@ -44,14 +44,17 @@
 
         <div style="height: 74px;"></div>
                 
-        <div class="container-fluid py-4">
+        <div class="container-fluid mt-4 py-4">
             <div class="row justify-content-center text-center">
                 <div class="col-sm-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#747d8c" class="bi bi-book-half" viewBox="0 0 16 16">      
-                        <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-                    </svg>
-                    <h1 class="style-blog-heading mt-2">Blog</h1>
-                    <p class="style-blog-note"></p>
+                    <div class="text-center mx-auto style-card-link-outer">
+                        <div class="border-0 shadow mx-auto style-card-link" style="background-color: #1abc9c;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-book-half style-card-link-icon" viewBox="0 0 16 16" aria-label="Blog">      
+                                <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                            </svg>
+                        </div>
+                        <div class="mt-2 style-card-link-text">Blog</div>
+                    </div>
                 </div>
             </div>
             <form method="GET" id="query">
--- portfolio.php
+++ portfolio.php
@@ -44,14 +44,17 @@
 
         <div style="height: 74px;"></div>
                 
-        <div class="container-fluid py-4">
+        <div class="container-fluid mt-4 py-4">
             <div class="row justify-content-center text-center">
                 <div class="col-sm-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#747d8c" class="bi bi-folder-fill" viewBox="0 0 16 16">
-                        <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
-                    </svg>
-                    <h1 class="style-portfolio-heading mt-2">Portfolio</h1>
-                    <p class="style-portfolio-note"></p>
+                    <div class="text-center mx-auto style-card-link-outer">
+                        <div class="border-0 shadow mx-auto style-card-link" style="background-color: #f1c40f;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-folder-fill style-card-link-icon" viewBox="0 0 16 16">
+                                <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
+                            </svg>
+                        </div>
+                        <div class="mt-2 style-card-link-text">Portfolio</div>
+                    </div>
                 </div>
             </div>
             <form method="GET" id="query">

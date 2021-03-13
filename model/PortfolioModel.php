2644e2d2a5fa4b09249117d5fec805fdbd33b6dd
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-03-14 00:00:38 +0800

    Update model

--- model/PortfolioModel.php
+++ model/PortfolioModel.php
@@ -19,6 +19,25 @@ class PortfolioModel{
         $this->link = $link;
     }
     
+    public function generateBadge($technologies_used){
+        $arr = explode(",", $technologies_used);
+        $badge = "";
+
+        $color_arr = array("#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#f1c40f", "#e67e22", "#e74c3c");
+        $color_counter = 0;
+
+        for($i=0; $i<count($arr); $i++){       
+            $badge .= "<span class='badge me-2 mt-2' style='background-color: ". $color_arr[$color_counter] .";'>". $arr[$i] . "</span>"; 
+            
+            $color_counter++;
+            if($color_counter >= count($color_arr)){
+                $color_counter = 0;
+            }
+        }
+
+        return $badge;
+    }
+
     public function getCard(){
         $card = "
         <div class='row justify-content-center mt-4'>
@@ -28,9 +47,8 @@ class PortfolioModel{
                         <img class='card-img-top img-fluid' alt='Banner' src='" . $this->image_banner . "' width='1024' height='500'>
                         <div class='card-body'>
                             <div class='card-title' style='font-size: 1.25rem; font-weight: 500;'>". $this->title ."</div>
-                            <p class='card-text' style='color: #747d8c;'>". $this->description ."</p>
-                            <div style='color: #0fbcf9; font-weight: 500;'>Technologies Used</div>
-                            <div style='color: #747d8c;'>". $this->technologies_used ."</div>
+                            <p style='color: #747d8c;'>". $this->description ."</p>
+                            ". $this->generateBadge($this->technologies_used) ."
                         </div>
                     </div>
                 </a>

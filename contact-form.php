c0665f8ed9095483c82b4c00b9f44eb7b74b1282
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-10-06 05:56:57 +0800

    Update contact-form design

--- contact-form.php
+++ contact-form.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Form Submission - Justine Paul Vitan</title>
+    <title>Form Submission | Justine Paul Vitan</title>
 
     <!-- Favicon -->
     <link rel="icon" href="https://jpvitan.com/favicon.ico">
@@ -17,7 +17,6 @@
     <!-- Meta Tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Get in touch with Justine Paul Vitan by sending him a message.">
     <meta name="author" content="Justine Paul Vitan">
 
     <!-- Bootstrap -->
@@ -74,26 +73,26 @@
                         <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='#2ecc71' class='bi bi-question-square-fill' viewBox='0 0 16 16'>
                             <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z'/>
                         </svg>
-                        <h1 class='lm-blue mt-4'>ARE YOU A ROBOT?</h1>
+                        <h1 class='mt-4'>Are You a Robot?</h1>
                         <p>If you are not a robot, please make sure that you have finished the reCAPTCHA before submitting.</p>
                         ";
                     $success_message = "
                         <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='#2ecc71' class='bi bi-check-circle-fill' viewBox='0 0 16 16'>
                             <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
                         </svg>
-                        <h1 class='lm-blue mt-4'>MESSAGE SENT!</h1>
+                        <h1 class='mt-4'>Message Sent!</h1>
                         <p>Hi, " . $full_name . "! Thanks for your message. Please expect a response soon from the email that you have provided.</p>
                         ";
                     $failed_message = "
                         <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='#ff3f34' class='bi bi-x-circle-fill' viewBox='0 0 16 16'>
                             <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/>
                         </svg>
-                        <h1 class='lm-blue mt-4'>FAILED TO SEND MESSAGE!</h1>
+                        <h1 class='mt-4'>Failed to Send Message!</h1>
                         <p>Unfortunately, your message was not sent. Please try again later.</p>
                         ";
 
-                    $ok_button = "<button class='btn mt-3 style-button' id='button' value='0'>OK</button>";
-                    $go_back_button = "<button class='btn mt-3 style-button' id='button' value='1'>Go Back</button>";
+                    $ok_button = "<button class='btn shadow-sm mt-3 style-button' id='button' value='0'>OK</button>";
+                    $go_back_button = "<button class='btn shadow-sm mt-3 style-button' id='button' value='1'>Go Back</button>";
 
                     $recaptcha_failed = false;
 

68495603174ef2d2f4de295e462ba32e71f731f3
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-11-01 00:23:32 +0800

    Fix code formatting

--- controller/BlogController.php
+++ controller/BlogController.php
@@ -2,67 +2,73 @@
 include "model/DatabaseHandler.php";
 include "model/BlogModel.php";
 
-class BlogController{
-    
-    public static function getAllData(){
+class BlogController
+{
+
+    public static function getAllData()
+    {
         $mysqli = DatabaseHandler::getConnection();
         $statement = $mysqli->prepare("SELECT * FROM blog ORDER BY date DESC");
         $data_array = self::compileToArray($statement);
         return $data_array;
     }
-    
-    public static function getDataFromTitle($title){
+
+    public static function getDataFromTitle($title)
+    {
         $mysqli = DatabaseHandler::getConnection();
         $statement = $mysqli->prepare("SELECT * FROM blog WHERE title LIKE ? ORDER BY date DESC");
-        $title.= "%";
+        $title .= "%";
         $statement->bind_param("s", $title);
         $data_array = self::compileToArray($statement);
         return $data_array;
     }
-    
-    public static function getDataFromCategory($category){
+
+    public static function getDataFromCategory($category)
+    {
         $mysqli = DatabaseHandler::getConnection();
         $statement = $mysqli->prepare("SELECT * FROM blog WHERE category=? ORDER BY date DESC");
         $statement->bind_param("s", $category);
         $data_array = self::compileToArray($statement);
         return $data_array;
     }
-    
-    public static function getDataFromTitleCategory($title, $category){
+
+    public static function getDataFromTitleCategory($title, $category)
+    {
         $mysqli = DatabaseHandler::getConnection();
         $statement = $mysqli->prepare("SELECT * FROM blog WHERE title LIKE ? AND category=? ORDER BY date DESC");
-        $title.= "%";
+        $title .= "%";
         $statement->bind_param("ss", $title, $category);
         $data_array = self::compileToArray($statement);
         return $data_array;
     }
-    
-    public static function getDataFromId($id){
+
+    public static function getDataFromId($id)
+    {
         $mysqli = DatabaseHandler::getConnection();
         $statement = $mysqli->prepare("SELECT * FROM blog WHERE id=?");
         $statement->bind_param("i", $id);
         $statement->execute();
         $statement->bind_result($id, $image_banner, $title, $description, $author, $date, $category, $link, $sub_category);
-        if($statement->fetch()){
+        if ($statement->fetch()) {
             return new BlogModel($id, $image_banner, $title, $description, $author, $date, $category, $link, $sub_category);
         }
         return null;
     }
-    
-    private static function compileToArray($statement){
+
+    private static function compileToArray($statement)
+    {
         $statement->execute();
         $statement->bind_result($id, $image_banner, $title, $description, $author, $date, $category, $link, $sub_category);
         $data_array = [];
-        while($statement->fetch()){
+        while ($statement->fetch()) {
             array_push($data_array, new BlogModel($id, $image_banner, $title, $description, $author, $date, $category, $link, $sub_category));
         }
         return $data_array;
     }
-    
-    public static function connectionWorking(){
+
+    public static function connectionWorking()
+    {
         $mysqli = DatabaseHandler::getConnection();
         return !$mysqli->connect_errno;
     }
-    
 }
-?>
--- model/DatabaseHandler.php
+++ model/DatabaseHandler.php
@@ -1,19 +1,20 @@
 <?php
-class DatabaseHandler{
-    
+class DatabaseHandler
+{
+
     private static $host = "localhost";
     private static $dbname = "jpvitan1_site";
 
     // For production server: 
-//    private static $username = "jpvitan1_master";
-//    private static $password = "!M,xxii*MKRR";
-    
+    // private static $username = "jpvitan1_master";
+    // private static $password = "!M,xxii*MKRR";
+
     // For local server:
     private static $username = "root";
-    private static $password = "";    
-    
-    public static function getConnection(){
+    private static $password = "";
+
+    public static function getConnection()
+    {
         return new mysqli(self::$host, self::$username, self::$password, self::$dbname);
     }
-    
 }

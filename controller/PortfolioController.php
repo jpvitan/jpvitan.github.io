7365b0cd5b7010041a7b68497977a1f00e5bb175
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-11-10 23:58:00 +0800

    Fix backend error for portfolio

--- controller/PortfolioController.php
+++ controller/PortfolioController.php
@@ -82,7 +82,9 @@ class PortfolioController
     {
         $mysqli = DatabaseHandler::getConnection();
         $status = !$mysqli->connect_errno;
-        $mysqli->close();
+        if ($status) {
+            $mysqli->close();
+        }
         return $status;
     }
 }

76b8196544d8290e37599ecc64a9dea63845586a
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-12-17 00:17:18 +0800

    Update font

--- blog-read.php
+++ blog-read.php
@@ -77,7 +77,7 @@ if (isset($_GET["id"])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>

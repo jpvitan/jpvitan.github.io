ab665359f9bfa0956f095c9bcb9e418fe52c1934
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-01-20 07:48:25 +0800

    Add shortcut icon

--- blog-read.php
+++ blog-read.php
@@ -22,6 +22,7 @@ if (isset($_GET["id"])) {
     <title><?php echo $blog->getTitle() . " - " . $blog->getAuthor(); ?></title>
 
     <!-- Favicon -->
+    <link rel="shortcut icon" href="https://jpvitan.com/favicon.ico">
     <link rel="icon" href="https://jpvitan.com/favicon.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
--- contact-form.php
+++ contact-form.php
@@ -5,6 +5,7 @@
     <title>Form Submission | Justine Paul Vitan</title>
 
     <!-- Favicon -->
+    <link rel="shortcut icon" href="https://jpvitan.com/favicon.ico">
     <link rel="icon" href="https://jpvitan.com/favicon.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">

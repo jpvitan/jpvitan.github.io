f2622797fd66259d7ee90b122cb750d9b13aa951
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-01-30 19:43:55 +0800

    Update faq.php

---
+++ faq.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>FAQ | Justine Paul Vitan</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="https://jpvitan.com/favicon.ico">
+    <link rel="icon" href="https://jpvitan.com/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Meta Tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="">
+    <meta name="author" content="Justine Paul Vitan">
+    <meta property="og:title" content="FAQ | Justine Paul Vitan" />
+    <meta property="og:image" content="https://jpvitan.com/img/banner.png" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="jpvitan.com" />
+
+    <!-- Twitter Cards -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@jpvitan_dev">
+    <meta name="twitter:creator" content="@jpvitan_dev">
+    <meta name="twitter:title" content="FAQ | Justine Paul Vitan">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="https://jpvitan.com/img/banner.png">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <!-- CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/faq.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="style-menu" id="menu"></div>
+
+    <div class="container-fluid fixed-top px-3 py-3 style-navbar" id="navbar">
+
+    </div>
+
+    <script>
+        $(function() {
+            $("#menu").load("menu.html");
+            $("#navbar").load("navbar.html");
+        });
+    </script>
+
+    <div class="py-5"></div>
+
+    <div style="overflow: hidden;">
+        <div class="container-fluid" style="background-color: #ffffff;">
+            <div class="row justify-content-center">
+                <div class="col-auto text-center">
+                    <div class="card border-0">
+                        <div class="card-body px-3 py-3 px-sm-5 py-sm-5">
+                            <div class="row justify-content-center text-start">
+                                <div class="col" style="max-width: 50rem;">
+                                    <div class="row">
+                                        <div class="col-md mx-md-2">
+                                            <h1>FAQ</h1>
+                                            <p style="font-weight: 700;">Justine Paul Vitan</p>
+                                            <p style="font-size: 1rem;">
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="filler"></div>
+
+    <footer class="pt-2" id="footer">
+
+    </footer>
+
+    <script>
+        let setFillerHeight = () => {
+            let filler = document.getElementById("filler");
+            let fillerY = $("#filler").offset().top;
+
+            let footer = document.getElementById("footer");
+            let footerHeight = footer.getBoundingClientRect().height;
+
+            let clientHeight = document.documentElement.clientHeight;
+
+            let fillerHeight = clientHeight - fillerY - footerHeight;
+
+            if (fillerHeight < 0) {
+                filler.style.height = 0 + "px";
+            } else {
+                filler.style.height = fillerHeight + "px";
+            }
+        }
+
+        window.addEventListener('resize', setFillerHeight);
+
+        $(function() {
+            $("#footer").load("footer.html", () => {
+                setFillerHeight();
+            });
+        });
+    </script>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- AOS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <!-- JavaScript -->
+    <script src="js/faq.js"></script>
+</body>
+
+</html>

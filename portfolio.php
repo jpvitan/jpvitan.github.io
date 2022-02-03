0d979e775a972706878cf7a8780f48aeb466a7e5
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-02-03 22:15:47 +0800

    Fix portfolio filler

--- portfolio.php
+++ portfolio.php
@@ -196,27 +196,27 @@
     </footer>
 
     <script>
-        let setFillerHeight = () => {
-            let filler = document.getElementById("filler");
-            let fillerY = $("#filler").offset().top;
-
-            let footer = document.getElementById("footer");
-            let footerHeight = footer.getBoundingClientRect().height;
-
-            let clientHeight = document.documentElement.clientHeight;
-
-            let fillerHeight = clientHeight - fillerY - footerHeight;
-
-            if (fillerHeight < 0) {
-                filler.style.height = 0 + "px";
-            } else {
-                filler.style.height = fillerHeight + "px";
-            }
-        }
-
-        window.addEventListener('resize', setFillerHeight);
-
         $(function() {
+            let setFillerHeight = () => {
+                let filler = document.getElementById("filler");
+                let fillerY = $("#filler").offset().top;
+
+                let footer = document.getElementById("footer");
+                let footerHeight = footer.getBoundingClientRect().height;
+
+                let clientHeight = document.documentElement.clientHeight;
+
+                let fillerHeight = clientHeight - fillerY - footerHeight;
+
+                if (fillerHeight < 0) {
+                    filler.style.height = 0 + "px";
+                } else {
+                    filler.style.height = fillerHeight + "px";
+                }
+            }
+
+            window.addEventListener('resize', setFillerHeight);
+
             $("#footer").load("footer.html", () => {
                 setFillerHeight();
             });

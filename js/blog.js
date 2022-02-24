ef40df6d06edcb317a8fa7dd0f98787471fedd2b
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-02-25 03:46:45 +0800

    Improve blog page semantics

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,29 @@
 AOS.init();
 
-window.onload = function () {
+$(function () {
+    $("#menu").load("menu.html");
+    $("#navbar").load("navbar.html");
 
-};
+    let setFillerHeight = () => {
+        let filler = document.getElementById("filler");
+        let fillerY = $("#filler").offset().top;
+
+        let footer = document.getElementById("footer");
+        let footerHeight = footer.getBoundingClientRect().height;
+
+        let clientHeight = document.documentElement.clientHeight;
+
+        let fillerHeight = clientHeight - fillerY - footerHeight;
+
+        if (fillerHeight < 0) {
+            filler.style.height = 0 + "px";
+        } else {
+            filler.style.height = fillerHeight + "px";
+        }
+    }
+
+    window.addEventListener('resize', setFillerHeight);
+    window.onload = setFillerHeight;
+
+    $("#footer").load("footer.html", setFillerHeight);
+});

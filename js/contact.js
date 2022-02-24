e74cca8718e055d11888737fd3b2a96846949237
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-02-25 07:31:51 +0800

    Remove JS on contact.html

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,18 @@
 AOS.init();
 
-window.onload = function () {
+$(function () {
+    $("#menu").load("menu.html");
+    $("#navbar").load("navbar.html");
+});
 
-};
+let toggleMeetingForm = (checked) => {
+    let meetingForm = document.getElementById("meetingForm");
+
+    document.getElementById("dateTime").required = checked;
+
+    if (checked) {
+        meetingForm.style.display = "block";
+    } else {
+        meetingForm.style.display = "none";
+    }
+}

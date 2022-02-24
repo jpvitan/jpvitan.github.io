ac4fbf5dd8d66a09463b4e7f93358c608645fbcf
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-02-25 00:40:22 +0800

    Improve faq page semantics

--- faq.php
+++ faq.php
@@ -61,98 +61,106 @@
         });
     </script>
 
-    <div class="py-5"></div>
+    <main>
+        <div class="py-5"></div>
 
-    <div style="overflow: hidden;">
-        <div class="container-fluid pb-5">
-            <div class="row justify-content-center">
-                <div class="col px-0 style-col-limiter">
-                    <div class="card border-0">
-                        <div class="card-body px-4 py-3 px-sm-5 py-sm-5">
-                            <h1>JUSTINE PAUL VITAN</h1>
-                            <p style="font-weight: 700;">Frequently Asked Questions</p>
-                            <p style="font-size: 1rem;">
-                                I'll answer the most frequently asked questions about my professional and technical background, interests, hobbies, and personal life.
-                            </p>
+        <section>
+            <div style="overflow: hidden;">
+                <div class="container-fluid pb-5">
+                    <div class="row justify-content-center">
+                        <div class="col px-0 style-col-limiter">
+                            <div class="card border-0">
+                                <div class="card-body px-4 py-3 px-sm-5 py-sm-5">
+                                    <h1>JUSTINE PAUL VITAN</h1>
+                                    <p style="font-weight: 700;">Frequently Asked Questions</p>
+                                    <p style="font-size: 1rem;">
+                                        I'll answer the most frequently asked questions about my professional and technical background, interests, hobbies, and personal life.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
 
-    <div style="overflow: hidden;">
-        <div class="container-fluid">
-            <div class="row justify-content-center">
-                <div class="col text-center px-0">
-                    <img class="img-fluid shadow" alt="Cat" src="https://res.cloudinary.com/dhv9gcew6/image/upload/q_auto/v1644324623/faq/cat_v1erjx.jpg" width="1920" height="1080">
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div style="overflow: hidden;">
-        <div class="container-fluid pb-5">
-            <div class="row justify-content-center">
-                <div class="col px-0 style-col-limiter">
-                    <div class="card border-0">
-                        <div class="card-body px-4 py-5 px-sm-5 py-sm-5">
-                            <h1>Personal Life</h1>
-                            <p style="font-weight: 700;">Tell Me About Yourself</p>
-                            <p style="font-size: 1rem;">
-                                I'm Justine Paul Vitan, a mobile and web developer based in the Philippines. I've been building and designing mobile and web applications as a hobby and profession. For the past few years, I've published many applications to prove my competency in developing and designing software.
-                            </p>
-                            <p style="font-size: 1rem;">
-                                Personality-wise, I love to describe myself as a tenacious person. I'm always insistent on finishing my goal, even if it gets to the point where I have to exhaust all my resources. This mindset allowed me to develop complex applications such as a satellite tracking app and a machine learning model that utilizes convolutional neural networks.
-                            </p>
-                            <p style="font-size: 1rem;">
-                                Moving forward, it is in my best interest to diversify, expand, and modernize my skill set to meet the demands of the industry. I'm always willing to learn and explore new things.
-                            </p>
+        <section>
+            <div style="overflow: hidden;">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col text-center px-0">
+                            <img class="img-fluid shadow" alt="Cat" src="https://res.cloudinary.com/dhv9gcew6/image/upload/q_auto/v1644324623/faq/cat_v1erjx.jpg" width="1920" height="1080">
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div style="overflow: hidden;">
-        <div class="container-fluid">
-            <div class="row justify-content-center">
-                <div class="col text-center px-0">
-                    <img class="img-fluid shadow" alt="Earth" src="https://res.cloudinary.com/dhv9gcew6/image/upload/q_auto/v1644324624/faq/earth_fiwped.jpg" width="1920" height="1080">
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div style="overflow: hidden;">
-        <div class="container-fluid pb-5">
-            <div class="row justify-content-center">
-                <div class="col px-0 style-col-limiter">
-                    <div class="card border-0">
-                        <div class="card-body px-4 py-5 px-sm-5 py-sm-5">
-                            <h1>Interests and Hobbies</h1>
-                            <p style="font-weight: 700;">What Are Your Interests and Hobbies?</p>
-                            <p style="font-size: 1rem;">
-                                Inside the realm of computer science, I'm interested in artificial intelligence, cryptography, and theoretical computer science. I've published several applications and blogs that revolve around these subdisciplines.
-                            </p>
-                            <blockquote class="twitter-tweet" data-theme="light">
-                                <p lang="en" dir="ltr">Currently experimenting with real-time object detection using SSD MobileNet v2. <a href="https://t.co/cJD3HlRXLD">pic.twitter.com/cJD3HlRXLD</a></p>&mdash; Justine Paul Vitan (@jpvitan_dev) <a href="https://twitter.com/jpvitan_dev/status/1417212472944054299?ref_src=twsrc%5Etfw">July 19, 2021</a>
-                            </blockquote>
-                            <p style="font-size: 1rem;">
-                                Outside computer science, I love to do amateur astronomy. I use a 900mm Newtonian reflector to observe the night sky and have seen several deep-sky objects and all the planets in the solar system except for Mercury and Neptune.
-                            </p>
-                            <blockquote class="twitter-tweet">
-                                <p lang="en" dir="ltr">Here&#39;s a photo of Mars that I took a year ago using my 900mm Newtonian reflector and 20mm eyepiece. 🔭 <a href="https://t.co/p6BRgpG31j">pic.twitter.com/p6BRgpG31j</a></p>&mdash; Justine Paul Vitan (@jpvitan_dev) <a href="https://twitter.com/jpvitan_dev/status/1442615763550965766?ref_src=twsrc%5Etfw">September 27, 2021</a>
-                            </blockquote>
+            <div style="overflow: hidden;">
+                <div class="container-fluid pb-5">
+                    <div class="row justify-content-center">
+                        <div class="col px-0 style-col-limiter">
+                            <div class="card border-0">
+                                <div class="card-body px-4 py-5 px-sm-5 py-sm-5">
+                                    <h1>Personal Life</h1>
+                                    <p style="font-weight: 700;">Tell Me About Yourself</p>
+                                    <p style="font-size: 1rem;">
+                                        I'm Justine Paul Vitan, a mobile and web developer based in the Philippines. I've been building and designing mobile and web applications as a hobby and profession. For the past few years, I've published many applications to prove my competency in developing and designing software.
+                                    </p>
+                                    <p style="font-size: 1rem;">
+                                        Personality-wise, I love to describe myself as a tenacious person. I'm always insistent on finishing my goal, even if it gets to the point where I have to exhaust all my resources. This mindset allowed me to develop complex applications such as a satellite tracking app and a machine learning model that utilizes convolutional neural networks.
+                                    </p>
+                                    <p style="font-size: 1rem;">
+                                        Moving forward, it is in my best interest to diversify, expand, and modernize my skill set to meet the demands of the industry. I'm always willing to learn and explore new things.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
 
-    <div id="filler"></div>
+        <section>
+            <div style="overflow: hidden;">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col text-center px-0">
+                            <img class="img-fluid shadow" alt="Earth" src="https://res.cloudinary.com/dhv9gcew6/image/upload/q_auto/v1644324624/faq/earth_fiwped.jpg" width="1920" height="1080">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div style="overflow: hidden;">
+                <div class="container-fluid pb-5">
+                    <div class="row justify-content-center">
+                        <div class="col px-0 style-col-limiter">
+                            <div class="card border-0">
+                                <div class="card-body px-4 py-5 px-sm-5 py-sm-5">
+                                    <h1>Interests and Hobbies</h1>
+                                    <p style="font-weight: 700;">What Are Your Interests and Hobbies?</p>
+                                    <p style="font-size: 1rem;">
+                                        Inside the realm of computer science, I'm interested in artificial intelligence, cryptography, and theoretical computer science. I've published several applications and blogs that revolve around these subdisciplines.
+                                    </p>
+                                    <blockquote class="twitter-tweet" data-theme="light">
+                                        <p lang="en" dir="ltr">Currently experimenting with real-time object detection using SSD MobileNet v2. <a href="https://t.co/cJD3HlRXLD">pic.twitter.com/cJD3HlRXLD</a></p>&mdash; Justine Paul Vitan (@jpvitan_dev) <a href="https://twitter.com/jpvitan_dev/status/1417212472944054299?ref_src=twsrc%5Etfw">July 19, 2021</a>
+                                    </blockquote>
+                                    <p style="font-size: 1rem;">
+                                        Outside computer science, I love to do amateur astronomy. I use a 900mm Newtonian reflector to observe the night sky and have seen several deep-sky objects and all the planets in the solar system except for Mercury and Neptune.
+                                    </p>
+                                    <blockquote class="twitter-tweet">
+                                        <p lang="en" dir="ltr">Here&#39;s a photo of Mars that I took a year ago using my 900mm Newtonian reflector and 20mm eyepiece. 🔭 <a href="https://t.co/p6BRgpG31j">pic.twitter.com/p6BRgpG31j</a></p>&mdash; Justine Paul Vitan (@jpvitan_dev) <a href="https://twitter.com/jpvitan_dev/status/1442615763550965766?ref_src=twsrc%5Etfw">September 27, 2021</a>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div id="filler"></div>
+    </main>
 
     <footer class="pt-2" id="footer">
 

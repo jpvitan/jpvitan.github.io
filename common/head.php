220071e51a89442db8f0f6fa86c82f096ab5c2de
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-08 01:01:10 +0800

    Add head.php

---
+++ common/head.php
@@ -0,0 +1,31 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="theme-color" content="#ffffff">
+<meta name="description" content="<?php echo $page->getDescription(); ?>">
+<meta name="author" content="<?php echo $page->getAuthor(); ?>">
+<link rel="manifest" href="https://jpvitan.com/site.webmanifest">
+<!-- Favicon -->
+<link rel="icon" href="https://jpvitan.com/favicon.ico">
+<link rel="apple-touch-icon" sizes="180x180" href="https://jpvitan.com/apple-touch-icon.png">
+<!-- Open Graph -->
+<meta property="og:url" content="<?php echo $page->getURL(); ?>" />
+<meta property="og:title" content="<?php echo $page->getTitle(); ?>" />
+<meta property="og:description" content="<?php echo $page->getDescription(); ?>" />
+<meta property="og:image" content="<?php echo $page->getImage(); ?>" />
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="<?php echo $page->getTwitter(); ?>">
+<meta name="twitter:creator" content="<?php echo $page->getTwitter(); ?>">
+<meta name="twitter:title" content="<?php echo $page->getTitle(); ?>">
+<meta name="twitter:description" content="<?php echo $page->getDescription(); ?>">
+<meta name="twitter:image" content="<?php echo $page->getImage(); ?>">
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+<!-- CSS -->
+<link href="css/main.css" rel="stylesheet">
+
+<title><?php echo $page->getTitle(); ?></title>

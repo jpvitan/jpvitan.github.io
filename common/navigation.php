2b8897e2f03cf2657f484b5f4ac69d1789673c7a
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-09 02:16:12 +0800

    Add navigation IDs

--- common/navigation.php
+++ common/navigation.php
@@ -1,9 +1,9 @@
 <div class="navigation">
-  <div class="bar">
+  <div class="bar" id="navigation-bar">
     <div class="container-fluid px-3 py-3">
       <div class="row">
         <div class="col-auto my-auto">
-          <button class="btn">
+          <button class="btn" id="navigation-menu-button">
             <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 1080 1080" width="20" height="20">
               <g transform="matrix(1 0 0 1 540 540)">
                 <rect style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1; visibility: hidden;" vector-effect="non-scaling-stroke" x="-540" y="-540" rx="0" ry="0" width="1080" height="1080" />
@@ -18,11 +18,11 @@
       </div>
     </div>
   </div>
-  <div class="menu">
+  <div class="menu" id="navigation-menu">
     <div class="container-fluid px-3 py-3">
       <div class="row">
         <div class="col-auto my-auto">
-          <button class="btn">
+          <button class="btn" id="navigation-close-button">
             <svg xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 16 16" width="20" height="20">
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
             </svg>

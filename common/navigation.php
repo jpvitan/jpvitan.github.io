213c8075fb3a1037542dd8a18f76bb894381d46f
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-11 01:14:00 +0800

    Adjust margin and padding

--- common/navigation.php
+++ common/navigation.php
@@ -29,21 +29,21 @@
           </button>
         </div>
       </div>
-      <div class="row justify-content-center mt-5">
+      <div class="row justify-content-center">
         <div class="col-auto my-auto">
           <div class="control-panel">
-            <div class="container-fluid px-3 py-3">
+            <div class="container-fluid px-0 py-5">
               <div class="row justify-content-center">
-                <div class="col-auto text-center">
+                <div class="col-md-auto text-center mt-md-5 px-5">
                   <a href="/">
-                    <div class="icon-container">
+                    <div class="icon-container mx-auto">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
                         <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
                       </svg>
                     </div>
                   </a>
-                  <p class="mt-2">Home</p>
+                  <p>Home</p>
                 </div>
               </div>
             </div>

60f97da711f106e3f0fe3607c966306192f29ed7
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-11 00:09:59 +0800

    Create control panel

--- common/navigation.php
+++ common/navigation.php
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-auto my-auto">
           <button class="button-svg btn" id="navigation-menu-button">
-            <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 1080 1080" width="20" height="20">
+            <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" fill="currentColor" viewBox="0 0 1080 1080">
               <g transform="matrix(1 0 0 1 540 540)">
                 <rect style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1; visibility: hidden;" vector-effect="non-scaling-stroke" x="-540" y="-540" rx="0" ry="0" width="1080" height="1080" />
               </g>
@@ -23,10 +23,31 @@
       <div class="row">
         <div class="col-auto my-auto">
           <button class="button-svg btn" id="navigation-close-button">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 16 16" width="20" height="20">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
               <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
             </svg>
           </button>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-5">
+        <div class="col-auto my-auto">
+          <div class="control-panel">
+            <div class="container-fluid px-3 py-3">
+              <div class="row justify-content-center">
+                <div class="col-auto text-center">
+                  <a href="/">
+                    <div class="icon-container">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
+                        <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
+                      </svg>
+                    </div>
+                  </a>
+                  <p class="mt-2">Home</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

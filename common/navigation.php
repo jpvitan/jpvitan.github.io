e6801c7dcedf48adf76a004349e6182f8771c072
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-12 00:24:55 +0800

    Use PHP to generate navigation tags

--- common/navigation.php
+++ common/navigation.php
@@ -1,5 +1,13 @@
 <?php
 include "utilities/Icon.php";
+include "utilities/Navigation.php";
+
+$navigationArray = array(
+  new Navigation(Icon::Home(), "Home", "/"),
+  new Navigation(Icon::App(), "Apps", "/"),
+  new Navigation(Icon::Blog(), "Blog", "/"),
+  new Navigation(Icon::Message(), "Message", "/")
+)
 ?>
 <div class="navigation">
   <div class="bar" id="navigation-bar">
@@ -27,49 +35,11 @@ include "utilities/Icon.php";
           <div class="control-panel">
             <div class="container-fluid px-0 py-5">
               <div class="row justify-content-center">
-                <div class="col-md-auto text-center mt-md-5 px-5">
-                  <a href="/">
-                    <div class="icon-container mx-auto">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
-                      </svg>
-                    </div>
-                  </a>
-                  <p>Home</p>
-                </div>
-                <div class="col-md-auto text-center mt-5 px-5">
-                  <a href="/">
-                    <div class="icon-container mx-auto">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z" />
-                        <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                      </svg>
-                    </div>
-                  </a>
-                  <p>Apps</p>
-                </div>
-                <div class="col-md-auto text-center mt-5 px-5">
-                  <a href="/">
-                    <div class="icon-container mx-auto">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
-                        <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
-                        <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
-                      </svg>
-                    </div>
-                  </a>
-                  <p>Blog</p>
-                </div>
-                <div class="col-md-auto text-center mt-5 px-5">
-                  <a href="/">
-                    <div class="icon-container mx-auto">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
-                      </svg>
-                    </div>
-                  </a>
-                  <p>Message</p>
-                </div>
+                <?php
+                foreach ($navigationArray as $navigation) {
+                  echo $navigation->generate();
+                }
+                ?>
               </div>
             </div>
           </div>

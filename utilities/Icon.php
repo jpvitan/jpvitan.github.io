fa55419228a9f759ae8e50f6288b2d71f3aedd1b
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-11 20:17:19 +0800

    Shorten svg tags

--- utilities/Icon.php
+++ utilities/Icon.php
@@ -5,4 +5,9 @@ class Icon
     {
         return '<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" fill="currentColor" viewBox="0 0 1080 1080"><g transform="matrix(1 0 0 1 540 540)"><rect style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1; visibility: hidden;" vector-effect="non-scaling-stroke" x="-540" y="-540" rx="0" ry="0" width="1080" height="1080" /></g><g transform="matrix(1 0 0 1 540 540)"></g><g transform="matrix(45 0 0 45 540 540)"><path style="stroke: rgb(0,0,0); stroke-width: 2; stroke-dasharray: none; stroke-linecap: round; stroke-dashoffset: 0; stroke-linejoin: round; stroke-miterlimit: 4; fill: none; fill-rule: nonzero; opacity: 1;" transform=" translate(-12, -12)" d="M 3 8.5 L 21 8.5 M 3 15.5 L 21 15.5" stroke-linecap="round" /></g></svg>';
     }
+
+    static function Close()
+    {
+        return '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" /></svg>';
+    }
 }

0ac644ba760d32a3a1c10ef9241607c90134619d
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2023-01-16 02:29:31 +0800

    Add ListCard

---
+++ utilities/ListCard.php
@@ -0,0 +1,28 @@
+<?php
+class ListCard
+{
+    private $icon;
+    private $title;
+    private $items;
+    private $marker;
+    private $style;
+
+    function __construct($icon, $title, $items, $marker, $style)
+    {
+        $this->icon = $icon;
+        $this->title = $title;
+        $this->items = $items;
+        $this->marker = $marker;
+        $this->style = $style;
+    }
+
+    function generate()
+    {
+        $itemRows = "";
+        foreach ($this->items as $item) {
+            $itemRows .= "<div class='item-row row mt-2'><div class='col-auto my-auto'>{$this->marker}</div><div class='col my-auto'><p>{$item}</p></div></div>";
+        }
+
+        return "<div class='list-card card shadow-lg border-0 px-5 py-5' style='{$this->style}'><div class='container-fluid px-0'><div class='icon-row row justify-content-center'><div class='col-auto my-auto'>{$this->icon}</div></div><div class='title-row row mt-4'><div class='col-auto my-auto'><h4>{$this->title}</h4></div></div>{$itemRows}</div></div>";
+    }
+}

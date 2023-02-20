614636e11fadadf949ac34a5cdb6af8ff50e59fc
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-02-21 02:37:08 +0800

    Add generate function

--- components/AppCard.php
+++ components/AppCard.php
@@ -17,4 +17,9 @@ class AppCard
         $this->description = $description;
         $this->style = $style;
     }
+
+    function generate()
+    {
+        return "<div class='app-card card border-0 px-4 px-md-5 py-5' style='{$this->style}'><img class='icon shadow mb-4' alt='{$this->title}' src='{$this->icon}' width='70' height='70'><div class='row'><div class='col my-auto'><h3>{$this->title}</h3><p class='fw-bold'>{$this->platform}</p></div><div class='col-auto my-auto'><a class='basic-button background-color-clouds font-color-peter-river btn' href='{$this->link}'>View</a></div></div><p>{$this->description}</p></div>";
+    }
 }

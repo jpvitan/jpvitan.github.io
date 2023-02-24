a4fb1a385cef5972bcb43178e567cceed0569432
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-02-25 03:02:36 +0800

    Handle theme-color on scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     setupNavigation()
+    setupThemeColor()
     AOS.init()
 })
 
@@ -10,18 +11,30 @@ const setupNavigation = () => {
     const closeButton = document.getElementById("navigation-close-button")
     let previousScrollPosition = window.pageYOffset
 
-    window.onscroll = () => {
+    window.addEventListener("scroll", () => {
         let currentScrollPosition = window.pageYOffset
 
         if (previousScrollPosition > currentScrollPosition && currentScrollPosition !== 0) bar.classList.remove("d-none")
         else bar.classList.add("d-none")
 
         previousScrollPosition = currentScrollPosition
-    }
+    })
     menuButton.onclick = () => {
         menu.classList.remove("d-none")
     }
     closeButton.onclick = () => {
         menu.classList.add("d-none")
     }
+}
+
+const setupThemeColor = () => {
+    const themeColor = document.querySelector("meta[name='theme-color']")
+    let defaultColor = themeColor.getAttribute("content")
+
+    window.addEventListener("scroll", () => {
+        let currentScrollPosition = window.pageYOffset
+
+        if (currentScrollPosition === 0) themeColor.setAttribute("content", defaultColor)
+        else themeColor.setAttribute("content", "#ffffff")
+    })
 }

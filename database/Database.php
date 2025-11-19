cea8dd29b9f61e6654134546f7578fc1a6a488a9
Justine Paul Vitan <jpvitanx@gmail.com>
2025-11-20 03:30:19 +0800

    Remove password

--- database/Database.php
+++ database/Database.php
@@ -4,7 +4,7 @@ class Database
     private static $host = "localhost";
     private static $dbname = "jpvitan1_website";
     private static $username = "jpvitan1_master";
-    private static $password = "!M,xxii*MKRR";
+    private static $password = "";
 
     public static function getConnection()
     {

b56a06fb5081895633e5f00b964f2653742d9df2
Justine Paul Vitan <jpvitanx@gmail.com>
2026-01-03 03:12:22 +0800

    Update Bootstrap to 5.3.8

--- src/common/head.php
+++ src/common/head.php
@@ -12,7 +12,7 @@
 <meta property="og:description" content="<?php echo $page->getDescription(); ?>" />
 <meta property="og:image" content="<?php echo $page->getImage(); ?>" />
 <!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
